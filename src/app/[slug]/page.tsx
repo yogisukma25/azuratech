3ac9335dfd89ffa45d0b9754fbae6aba7d734906
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2024-05-21 01:32:06 -0500

    static paths

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -3,6 +3,34 @@ import type { PageProps } from "@/types";
 
 import { Container } from "@/components/Container";
 
+const pages = {
+  data: [
+    {
+      id: 1,
+      slug: "features",
+    },
+    {
+      id: 2,
+      slug: "pricing",
+    },
+    {
+      id: 3,
+      slug: "company",
+    },
+  ],
+};
+
+interface StaticParamsProps {
+  id: number;
+  slug: string;
+}
+
+export async function generateStaticParams() {
+  return pages.data.map((page: Readonly<StaticParamsProps>) => ({
+    slug: page.slug,
+  }));
+}
+
 export default function DynamicPageRoute(props: Readonly<PageProps>) {
   const slug = props.params?.slug;
   return (
