3c01f5823b9019aa28c70d4b470420a749b1f9e4
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2024-05-21 14:43:30 -0500

    update

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,7 +5,7 @@ import { Container } from "@/components/Container";
 export default function DynamicPageRoute() {
   return (
     <Container>
-      <div>Blog Static Page</div>
+      <div>Blog Static Route</div>
     </Container>
   );
 }
