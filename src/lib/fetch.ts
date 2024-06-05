ce07a94a798024c3c42df2314e0679e323a4f504
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2024-06-04 19:32:43 -0500

    update

---
+++ src/lib/fetch.ts
@@ -0,0 +1,22 @@
+import { flattenAttributes } from "@/lib/utils";
+
+export async function fetchData(url: string, authToken?: string) {
+
+  const headers = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${authToken}`,
+    },
+  };
+
+  try {
+    const response = await fetch(url, authToken ? headers : {});
+    const data = await response.json();
+    if (!response.ok) throw new Error("Failed to fetch data");
+    return flattenAttributes(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error; // or return null;
+  }
+}
